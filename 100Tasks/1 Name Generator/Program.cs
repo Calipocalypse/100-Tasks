49b820dc75328f26599f45e28e936ce71ae31fe6
Maciej Ziółkowski <maciej.ziolkowski@microsoft.wsei.edu.pl>
2021-02-22 20:15:07 +0100

    01 Name Generator | Added

---
+++ 100Tasks/1 Name Generator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _100Tasks._1_Name_Generator
+{
+    public static class Program
+    {
+        public static void Run()
+        {
+            Console.Write("How many names you want to generate: ");
+            int numberOfGenerations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfGenerations; i++)
+            {
+                string fullName = GenerateName();
+                Console.WriteLine("Generated full name: " + fullName);
+            }
+        }
+
+        private static string GenerateName()
+        {
+            string generatedFullName;
+            // Randomize for first name
+            Random randomized = new Random();
+            int random = randomized.Next(0, Enum.GetNames(typeof(Name)).Length);
+            generatedFullName = ((Name)random).ToString();
+
+            // Randomize for last name
+            random = randomized.Next(0, Enum.GetNames(typeof(LastName)).Length);
+            generatedFullName += " " + ((LastName)random).ToString();
+
+            return generatedFullName;
+        }
+
+        enum Name
+        {
+            Maciej, Rafał, Bartłomiej, Beniamin, Piotr, Dominik, Roman, Krystian, Albert, Witold, Mateusz, Wiktor, Paweł
+        }
+
+        enum LastName
+        {
+            Ziółkowski, Walipięta, Rozwadowski, Gwiazdowski, Lepiński, Wileński, Magdeburski, Wisłocki, Kupiec, Woźniak
+        }
+    }
+}
--- 100Tasks/Program.cs
+++ 100Tasks/Program.cs
@@ -6,7 +6,21 @@ namespace _100Tasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GIT!");
+            int timeNowInHours = DateTime.Now.Hour;
+            Console.WriteLine("Good " + GetWelcomeWord(timeNowInHours) + ". Which program you want to run right now?");
+            _1_Name_Generator.Program.Run();
         }
+
+        static string GetWelcomeWord(int hour)
+        {
+            if (hour >= 6 && hour < 12) return Day.Morning.ToString();
+            else if (hour >= 12 && hour < 18) return Day.Afternoon.ToString();
+            else return Day.Evening.ToString();
+        }
+    }
+
+    enum Day
+    {
+        Morning, Afternoon, Evening
     }
 }
